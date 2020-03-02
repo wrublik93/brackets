ed6d5a1205817a85ba28f0cf744aabf603d782a3
wrublik93 <wrublik.93@mail.ru>
2020-03-03 00:26:11 +0300

    feat: implement function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    /*parity check*/
+  if(str.length % 2 != 0 || str.length == 0) {
+      return false;
+  } else {
+      for(let i = 0; i < bracketsConfig.length; ){
+          /*finding the kind of brackets*/
+          let bracketKind = bracketsConfig[i].join('');
+          if(str.includes(bracketKind)){
+              str = str.replace(bracketKind, '');
+              i = 0;
+          } else {
+              i = i + 1;
+          }
+      };
+      if(str.length) {
+          return false;
+      } else {
+          return true;
+      }
+  }
 }
